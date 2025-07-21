07da8a85a8bc6ceacb3f6f64286cf97e135c9dfe
Entity146 <entityendless47@gmail.com>
2025-07-21 10:26:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   SESSION_ID: "SUBZERO-MD;;;vAVijJ5a#ufdpyiNrxN1h4UPGYsFpL5xE2PzTXaFJl4i2RIvz470", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "+2348074553904", // Put Owner Number Here 
+  OWNER_NUMBER: "+2348074553904", // 08074553904
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
